8cbff2efba3ebf57f04d31bc100159fe934fb90e
Garrett West <gwest@dev-10.com>
2022-05-15 12:55:24 -0400

    Assessment 8 - Delete Functioning

--- assessment08/src/Agents.js
+++ assessment08/src/Agents.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 function Agents() {
     const [agents, setAgents] = useState([]);
-    const [editIndex, setEditIndex] = useState();
+    const [editIndex, setEditIndex] = useState(null);
     const [formData, setFormData] = useState({
         firstName:"",
         middleName:"",
@@ -17,28 +17,91 @@ function Agents() {
         })
     }, []);
 
-    const handleDelete = () => {
+    const handleEdit = (id) => {
+        console.log(`Edits ${id}`);
+        setEditIndex(id);
+        setFormData();
+    }
+
+    const handleDelete = (id) => {
         console.log(`Deletes ${id}`);
+        fetch(`http://localhost:8080/api/agent/${id}`, {
+            method: "DELETE"
+        })
+        .then(resp => {
+            const newAgents = agents.filter(agent => agent.agentId !== id);
+            setAgents(newAgents);
+        });
+    };
+
+    const handleFormChange = (e) => {
+        setFormData(e.target.value);
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        if(editIndex){
+    //     e.preventDefault();
+    //     if(editIndex){
+    //         console.log(`currently editing ${editIndex}`)
+    //     } else {
+    //         console.log("add a new agent");
+    //         const agent = {
+    //             firstName,
+    //             middleName,
+    //             lastName
+    //         };
 
-        }
+    //         const init = {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             body: JSON.stringify(agent)
+    //         }
+    //         fetch("http://localhost:8080/api/agent", init)
+    //         .then(response => {
+    //             if (response.status == 201) {
+    //                 setAgents("");
+    //             } else {
+    //                 return Promise.reject("POST agent status was not 201");
+    //             }
+    //         })
+    //         .catch(console.error);
+    //     }
     }
 
-    return (
-    <div>
+    return (<div>
         <h3>Agents</h3>
         <hr/>
+        <h2>Display Agent</h2>
+        <div>
             {
                 agents.map(a => {
                     return(
-                    <div>
-                        <span>{a.firstName} {a.lastName} <button>Edit</button></span>
-                    <div/>)
+                        <p>{a.firstName} {a.middleName} {a.lastName} <button onClick={() => (handleEdit(a.agentId))}>Edit</button><button onClick={() => (handleDelete(a.agentId))}>Delete</button></p>
+                        )
                 })
             }
+        </div>
+        <hr/>
+        <div>
+            <h2>{(editIndex)?"Edit":"Create"} Agent</h2>
+            <form>
+                <div>
+                    <label htmlFor='firstName'>First Name</label>
+                    <input id='firstName'onChange={handleFormChange} name='firstName' type='text'/>
+                </div>
+                <div>
+                    <label htmlFor='middleName'>Middle Name</label>
+                    <input id='middleName'onChange={handleFormChange} name='middleName' type='text'/>
+                </div>
+                <div>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input id='lastName' onChange={handleFormChange} name='lastName' type='text'/>
+                </div>
+                <button onClick={handleSubmit}>{(editIndex)?"Edit":"Create"} Agent</button>
+            </form>
+        </div>
     </div>)
 }
+
+export default Agents;
